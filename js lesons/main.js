c2f3d68f611a1a463b18ef2360f6c9c2ef29b994
Alexandr <babiyksasha21@gmail.com>
2021-11-29 20:35:02 +0200

    my first program in udemy

---
+++ js lesons/main.js
@@ -0,0 +1,20 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Один из последних просмотрених фильмов', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+    //   personalMovieDB.movies[a] = b;
+
+    console.log(personalMovieDB);
